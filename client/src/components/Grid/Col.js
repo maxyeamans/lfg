b40831b6e2c13dea58f940216ca0d25164b06a9a
DanielG <Danielmorrisg@gmail.com>
2018-09-27 21:41:16 -0700

    added styles for react bootstrap

--- client/src/components/Grid/Col.js
+++ client/src/components/Grid/Col.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const Col = ({ size, children }) => (
-  <div className={size.split(" ").map(size => "col-" + size).join(" ")}>
+export const Col = ({ size, children , style = {}}) => (
+  <div style={style} className={size.split(" ").map(size => "col-" + size).join(" ")}>
     {children}
   </div>
 );
