e5442d836c4424e600752b97da6bccb00894f8fe
DanielG <Danielmorrisg@gmail.com>
2018-09-29 13:57:44 -0700

    added additional pages & react routes

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../../components/Grid";
-import SearchList from "../../components/SearchList";
 import SearchBtn from "../../components/SearchBtn";
 import CreateGroupBtn from "../../components/CreateGroupBtn";
 import CurrentGroups from "../../components/CurrentGroups";
 import Profile from "../../components/Profile";
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
 
 class Main extends Component {
   constructor(props) {
@@ -27,24 +28,24 @@ class Main extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-
   render() {
     return (
       // NAV IS RIGHT HERE
       <Container fluid>
+        <Nav />
         <Row>
           {/* Start Column 1 */}
           <Col size="xl-6 sm-6">
-              <h1>Groups</h1>
-              <CurrentGroups />
-              {/* PROPS NEEDED: Group Name, Members, Group Image */}
+            <h1>Groups</h1>
+            <CurrentGroups />
+            {/* PROPS NEEDED: Group Name, Members, Group Image */}
           </Col>
           {/* Start Column 2 */}
           <Col size="xl-6 sm-6">
             <Row>
               <Profile />
             </Row>
-            
+
             <Row>
               <Col size="xl-6 sm-6">
                 <CreateGroupBtn />
@@ -55,6 +56,7 @@ class Main extends Component {
             </Row>
           </Col>
         </Row>
+        <Footer />
       </Container>
     );
   }
---
+++ client/src/pages/Search/Search.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../../components/Grid";
+import SearchBar from "../../components/SearchBar";
+import SearchPlayer from "../../components/SearchPlayer";
+import SearchGroup from "../../components/SearchGroup";
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+import "./Search.css";
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  handleInputChange = event => {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  _onChange = event => {
+    event.preventDefault();
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    return (
+      // NAV IS RIGHT HERE
+      <Container fluid>
+      <Nav />
+        <Row>
+          <SearchBar />
+        </Row>
+
+        <Row>
+          <div id="search-results">
+            {/* Will need to find a way to determine which component to render, based on search filter of Players or Groups */}
+            <SearchPlayer />
+            <SearchGroup />
+          </div>
+        </Row>
+        <Footer />
+      </Container>
+    );
+  }
+}
+
+export default Search;
