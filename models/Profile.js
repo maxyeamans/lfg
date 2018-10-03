9ae32c2ff24d3ad147d0102735aa3a7b2f6a25d1
Michael Schumann <schumann82@gmail.com>
2018-10-02 20:51:36 -0700

    simplified schema

--- models/Profile.js
+++ models/Profile.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
+// const mongoose = require("mongoose");
+// const Schema = mongoose.Schema;
 
-const profileSchema = new Schema({
-  gamertag: { type: String, required: true, unique: true },
-  platform: { type: String, required: true },
-  role: { type: String, required: true },
-  group: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
+// const profileSchema = new Schema({
+//   gamertag: { type: String, required: true, unique: true },
+//   platform: { type: String, required: true },
+//   role: { type: String, required: true },
+//   group: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
   
-});
+// });
 
-const Profile = mongoose.model("Profile", profileSchema);
+// const Profile = mongoose.model("Profile", profileSchema);
 
-module.exports = Profile;
+// module.exports = Profile;
--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   email: { type: String, required: true },
-  profile: [{ type: Schema.Types.ObjectId, ref: 'Profile' }]
+  group: [{ type: Schema.Types.ObjectId, ref: 'Group' }]
   
 });
 
