49868e98ea3ef97e4e2b51d47d6ad5d346795a89
DanielG <Danielmorrisg@gmail.com>
2018-10-03 18:55:49 -0700

    minor changes to modal

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -3,7 +3,8 @@ import "./SearchBar.css";
 
 const SearchBar = props => (
 
-  <input className="form-control search-bar my-3"type="text" placeholder="Search" aria-label="Search"/>
+  <input className="form-control search-bar my-3" type="text" placeholder="Search" aria-label="Search"/>
+  
 
 );
 
