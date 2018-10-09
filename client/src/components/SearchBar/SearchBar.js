83e7a9c0664930f3926d499dddabf927f3a392b5
Katri Morrison-Goulias <katrimorrison-goulias@Konstadinoss-MacBook-Air.local>
2018-10-09 00:01:23 -0700

    added search and updated styles

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,58 +1,68 @@
 import React from "react";
 import "./SearchBar.css";
 
-const SearchBar = props => (
+ 
 
-  // <input className="form-control search-bar my-3" type="text" placeholder="Search" aria-label="Search"/>
-  <div className="container">
-	<div className="row" id="search">
-		<form id="search-form" action="" method="POST" enctype="multipart/form-data">
-			<div className="form-group col-xs-9">
-				<input className="form-control" type="text" placeholder="Search" />
-			</div>
-			<div class="form-group col-xs-3">
-				<button type="submit" className="btn btn-block btn-primary">Search</button>
-			</div>
-		</form>
-	</div>
-	<div className="row" id="filter">
-		<form>
-			<div className="form-group col-sm-3 col-xs-6">
-				<select data-filter="make" className="filter-make filter form-control">
-					<option value="">Select Platform</option>
-					<option value="">PC</option>
-          <option value="">XBox</option>
-          <option value="">Playstation</option>
-				</select>
-			</div>
-			<div className="form-group col-sm-3 col-xs-6">
-				<select data-filter="model" className="filter-model filter form-control">
-					<option value="">Select Role</option>
-					<option value="">Tank</option>
-          <option value="">Damage</option>
-          <option value="">Support</option>
-				</select>
-			</div>
-			<div className="form-group col-sm-3 col-xs-6">
-				<select data-filter="type" className="filter-type filter form-control">
-					<option value="">Select Rank (7)</option>
-					<option value="">Bronze</option>
-          <option value="">Silver</option>
-          <option value=""> Gold</option>
-          <option value="">Platinum</option>
-          <option value="">Diamond</option>
-          <option value="">Master</option>
-          <option value="">Grand Master</option>
-				</select>
-			</div>
-			
-		</form>
-	</div>
-	<div className="row" id="products">
+const SearchBar = props =>
+
+<div className="container">
+<div className="search-box">
+  <div className="row" >
+	<div className="col-lg-12" id ="search-bar">
+	  <div className="panel panel-primary">
 		
+		<div className="panel-body">
+		  <form>
+			<div className="form-group">
+			  <label htmlFor="topic">Group Name</label>
+			  <input onChange={props.handleGroupNameChange} type="text" className="form-control" id="topic" aria-describedby="emailHelp" />
+			</div>
+			<div className="form-group">
+			  <label htmlFor="start-year">Platform</label>
+			  <input onChange={props.handlePlatformChange} type="text" className="form-control" id="start-year" />
+			</div>
+			<div className="form-group">
+			  <label htmlFor="end-year">Group Rank</label>
+			  <input onChange={props.handleGroupRankChange} type="text" className="form-control" id="end-year" />
+			</div>
+			<div className="form-group">
+			  <label htmlFor="end-year">Time</label>
+			  <input onChange={props.handleTimeChange} type="text" className="form-control" id="end-year" />
+			</div>
+			<button onClick={props.handleFormSubmit} type="submit" className="submit-button">Search</button>
+		  </form>
+		</div>
+	  </div>
 	</div>
-</div>
+  </div>
+  </div>
 
-);
+  <br/><br/>
 
+  <div className="row">
+      <div className="col-lg-12">
+        <div className="panel panel-primary">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+              <strong>
+                <i className="fa fa-newspaper-o" aria-hidden="true"></i> Results
+              </strong>
+            </h3>
+          </div>
+          <div className="panel-body">
+			{/* {props.renderSearchedGroups()} */}
+			
+          </div>
+        </div>
+      </div>
+    </div>
+    <br/><br/>
+  
+
+
+ 
+ 
+ </div>
+
+  
 export default SearchBar;
--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -16,6 +16,8 @@ class Search extends Component {
     };
   }
 
+
+
   componentDidMount() {
     this.loadGroups();
   }
@@ -40,24 +42,71 @@ class Search extends Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  // //for rendering search groups on searchgroup component
+  // renderSearchedGroups = () => {
+  //   return this.state.groups.map(group => (
+  //     <SearchGroup
+  //       groupName={group.groupName}
+  //       platform={group.platform}
+  //       groupRank={group.groupRank}
+  //       time={group.time}
+  //     />
+  //   ));
+  // }
+
+  // // Keep track of what user types into topic input so that input can be grabbed later
+  // handleGroupNameChange = (event) => {
+  //   this.setState({ groupName: event.target.value });
+  // }
+
+  // // Keep track of what user types into topic input so that input can be grabbed later
+  // handlePlaformChange = (event) => {
+  //   this.setState({ platform: event.target.value });
+  // }
+
+  // // Keep track of what user types into topic input so that input can be grabbed later
+  // handleGroupRankChange = (event) => {
+  //   this.setState({ groupRank: event.target.value });
+  // }
+
+  // // Keep track of what user types into topic input so that input can be grabbed later
+  // handleTimeChange = (event) => {
+  //   this.setState({ time: event.target.value });
+  // }
+
+
+
   render() {
     return (
       // NAV IS RIGHT HERE
       <Container fluid>
         <Row>
-          <Col size="xl-4 lg-3" />
-          <Col size="xl-4 lg-6">
-          <h1>Search for a group below</h1>
-            <SearchBar />
+          
+          <Col size="xl-12 lg-6">
+          <div id="search-groups">
+            <h4>Search for a group below</h4>
+            <SearchBar
+            // handleGroupNameChange={this.handleTopicChange}
+            // handlePlatformChange={this.handleStartYearChange}
+            // handleGroupRankChange={this.handleEndYearChange}
+            // handleTimeChange={this.handleTimeChange}
+            // handleFormSubmit={this.handleFormSubmit}
+            // renderSearchedGroups={this.renderArticles}
+            />
+          </div>
+          <h5> ▼ </h5>
+
           </Col>
-          <Col size="xl-4 lg-3" />
+          
         </Row>
 
+
         <Row>
-          <Col size="xl-4 lg-3" />
-          <Col size="xl-4 lg-6">
+        
+          <Col size="xl-12 lg-6">
             <div id="search-results">
-            {this.state.groups.map(group => (
+              <h4> results</h4>
+              {this.state.groups.map(group => (
                 <SearchGroup
                   data={group}
                   groupName={group.groupName}
@@ -68,7 +117,7 @@ class Search extends Component {
               ))}
             </div>
           </Col>
-          <Col size="xl-4 lg-3" />
+          
         </Row>
         <Footer />
       </Container>
