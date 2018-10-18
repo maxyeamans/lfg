11630b0fbb5f642b724b30c4ec209b36eca7f1a8
Michael Schumann <schumann82@gmail.com>
2018-10-17 18:27:35 -0700

    group messaging

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import NewGroup from "./pages/NewGroup";
 import Register from "./pages/Register";
 import UpdateGroup from "./pages/UpdateGroup";
 import Footer from "./components/Footer";
+import Messages from "./pages/Messages";
 
 const App = () => (
   <Router>
@@ -20,6 +21,7 @@ const App = () => (
         <Route exact path="/main" component={Main} />
         <Route exact path="/search" component={Search} />
         <Route exact path="/newgroup" component={NewGroup} />
+        <Route exact path="/messages" component={Messages}/>
         <Route path="/updategroup/:id" component={UpdateGroup} />
         <Route component={NoPage} />
       </Switch>
